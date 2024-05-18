ca68bae0cf648eabfd151323cc3dc930d4084f78
Evgenii Viriasov <eviriasov@madela.dev>
2024-05-18 17:44:48 +0300

    added description

--- src/main/java/org/mcs/petproductservice/controller/ProductController.java
+++ src/main/java/org/mcs/petproductservice/controller/ProductController.java
@@ -1,9 +1,14 @@
 package org.mcs.petproductservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.mcs.petproductservice.dto.ProductRequestDto;
 import org.mcs.petproductservice.dto.ProductResponseDto;
 import org.mcs.petproductservice.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,31 +17,43 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/product-service")
 @RequiredArgsConstructor
+@Tag(name = "ProductController", description = "Контроллер товара")
 public class ProductController {
 
     private final ProductService productService;
 
     @GetMapping("/all")
+    @Operation(summary = "Получение всех товаров")
+    @ResponseStatus(HttpStatus.OK)
     public List<ProductResponseDto> getAllProducts() {
         return productService.getAllProducts();
     }
 
     @GetMapping("/product/{productId}")
-    public ProductResponseDto getProductByProductId(@PathVariable("productId") UUID productId) {
+    @Operation(summary = "получение описания товара по его id")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductResponseDto getProductByProductId(@Parameter(description = "id товара") @PathVariable("productId") UUID productId) {
         return productService.getProductById(productId);
     }
 
     @PostMapping("/add")
-    public void addProduct(@RequestBody ProductRequestDto productRequestDto) {
+    @Operation(summary = "добавление нового товара")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addProduct(@Valid @RequestBody ProductRequestDto productRequestDto) {
         productService.addProduct(productRequestDto);
     }
 
     @PatchMapping("/quantity/delete/{productId}")
-    public void deleteProductQuantityById(@PathVariable("productId") UUID productId, @RequestParam Integer quantity){
+    @Operation(summary = "удалить определеннное кол-во товаров из базы")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteProductQuantityById(@Parameter(description = "id товара")@PathVariable("productId") UUID productId,
+                                          @Parameter(description = "кол-во которое нужно удалить из базы") @RequestParam Integer quantity){
         productService.deleteProductQuantityById(productId, quantity);
     }
 
     @DeleteMapping("/delete/{productId}")
+    @Operation(summary = "полностью удалить товар из базы")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteProductById(@PathVariable("productId") UUID productId){
         productService.deleteProductById(productId);
     }
--- src/main/java/org/mcs/petproductservice/dto/ProductRequestDto.java
+++ src/main/java/org/mcs/petproductservice/dto/ProductRequestDto.java
@@ -1,5 +1,7 @@
 package org.mcs.petproductservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -9,8 +11,18 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @Builder
+@Schema(description = "описание товара")
 public class ProductRequestDto {
+
+    @Schema(description = "название товара")
+    @NotBlank(message = "название товара не указано")
     private String name;
+
+    @Schema(description = "описание товара")
+    @NotBlank(message = "описание товара не указано")
     private String description;
+
+    @Schema(description = "цена товара")
+    @NotBlank(message = "цена товара не указана")
     private BigDecimal price;
 }
